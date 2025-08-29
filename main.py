944765513bc9ab2bdec607724483b96d978f5c0c
LuissyS77 <luissyhaimar1@gmail.com>
2025-08-29 15:13:28 -0500

    ahora si deja usar bien el post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 import joblib 
 
@@ -7,12 +8,18 @@ columnas = joblib.load('columnas_modelo.pkl')
 
 app = FastAPI(title="Prediccion precio de Ventas")
 
+# Modelo de entrada
+class InputData(BaseModel):
+    empleados: float
+    publicidad: float
+    ubicacion: float
+    
 @app.get("/")
 def home():
     return {"message": "Bienvenido a la API de predicción de ventas"}
 
 @app.post("/predict/")
-def predict(empleados: float, publicidad: float, ubicacion: float):
-    x_new = pd.DataFrame([[empleados, publicidad, ubicacion]], columns=columnas)
+def predict(data: InputData):
+    x_new = pd.DataFrame([[data.empleados, data.publicidad, data.ubicacion]], columns=columnas)
     prediccion = model.predict(x_new)
-    return {"prediccion": prediccion[0]}
+    return {"prediccion": float(prediccion[0])}
